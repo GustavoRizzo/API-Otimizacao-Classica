c7b02bf6b4c6682948fc700a2849a7e061d0be97
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2019-07-14 15:45:50 -0300

    melhorando forma de escrever a funçôes matemáticas, permitindo uma linguagem mais próxima da natural da matemática

--- OtimizacaoClassicaLib/Otimizador.cs
+++ OtimizacaoClassicaLib/Otimizador.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using org.mariuszgromada.math.mxparser;
 
 
 namespace OtimizacaoClassicaLib
@@ -6,11 +7,25 @@ namespace OtimizacaoClassicaLib
     public class Otimizador
     {
         public static double declaraFuncao( double x)
-        {
+        {          
+            string num_str = doubleToStr(x);
+
+            //Escrevendo argumento
+            string x1_str = $"x1 = {num_str}";
+            Argument x1 = new Argument(x1_str);
+
             // Até o momento usaremos sempre a mesma função 
-            Console.WriteLine( "função criada: " + "0.65 -(0.75/(1+ Math.Pow(x,2))) - 0.65 * x * Math.Atan(1/x)"  );
-            double res = 0.65 -(0.75/(1+ Math.Pow(x,2))) - 0.65 * x * Math.Atan(1/x);
+            Function f = new Function("f(x) = 0.65 - (0.75 / (1 + x^2)) - 0.65 * x * atan(1/x)");
+            org.mariuszgromada.math.mxparser.Expression e = new org.mariuszgromada.math.mxparser.Expression("f(x1)", f, x1);
+            double res = e.calculate();
+
             return res;
+        }
+
+        // convertendo double em string, mantendo a pontuação correta
+        private static string doubleToStr( double d)
+        {
+            return d.ToString().Replace(',','.');
         }
     }
 }
