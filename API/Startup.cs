c8ef68d3120f75f7d839c2c25f6de15f2051b530
Gustavo Rizzo Albuquerque <grizzo.albu@gmail.com>
2019-09-14 19:55:41 -0300

    adição do swagger, de forma bem básica

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -35,7 +36,22 @@ namespace API
                     BuilderExtensions.WithOrigins("*");
                 });
             });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "My API",
+                    Version = "v1", 
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Gustavo Rizzo",
+                        Email = "grizzo.albu@gmail.com",
+                        Url = new Uri("https://github.com/GustavoRizzo/API-Otimizacao-Classica"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +66,13 @@ namespace API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Otimização Classica");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseCors(MyAllowSpecificOrigins);
 
